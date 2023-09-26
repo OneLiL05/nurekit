0f1e5722ce5685779ca3e3f89268e8329cbe7b2a
OneLiL05 <k_savieliev@icloud.com>
2023-09-26 19:44:21 +0200

    feat: improve performance

--- packages/nurekit/src/helpers/schedule.helper.ts
+++ packages/nurekit/src/helpers/schedule.helper.ts
@@ -2,17 +2,20 @@ import { IRawSchedule, ISchedule } from "../index.js";
 import { transformTeachers } from "./teachers.helper.js";
 
 export function transformSchedule(rawSchedule: IRawSchedule[]): ISchedule[] {
-	return rawSchedule.map((rawSchedule) => {
-		const { end_time, start_time, teachers, number_pair, auditory, ...rest } =
-			rawSchedule;
+	const transformedSchedule: ISchedule[] = [];
 
-		return {
+	for (let subject of rawSchedule) {
+		const { end_time, start_time, teachers, number_pair, auditory, ...rest } = subject;
+
+		transformedSchedule.push({
 			...rest,
 			endTime: end_time,
 			startTime: start_time,
 			teachers: transformTeachers(teachers),
 			numberPair: number_pair,
 			auditorium: auditory,
-		};
-	});
+		});
+	}
+
+	return transformedSchedule;
 }
